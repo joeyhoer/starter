a4f3dd0be09d1d24ebf4974154aecf92bfaf0c21
Joey Hoer <joey@gaugeinteractive.com>
2014-11-19 13:31:46 -0500

    Use mogrify instead of convert to edit images in place

--- apps/copyclip.sh
+++ apps/copyclip.sh
@@ -8,4 +8,4 @@
 defaults write com.fiplab.clipboard FirstRun -bool false
 
 # Invert menubar icon colors, for use with dark mode
-# sudo find /Applications/CopyClip.app/Contents/Resources/ -name icon-statusbar* -exec convert -negate "{}" "{}" \;
+# sudo find /Applications/CopyClip.app/Contents/Resources/ -name icon-statusbar* -exec mogrify -negate "{}" \;
