d978a4e05a57965cfaed04aaecdfeecf85587e02
Joey Hoer <me@joeyhoer.com>
2014-11-18 20:24:10 -0500

    Adding more defaults for Quicktime

--- apps/quicktime.sh
+++ apps/quicktime.sh
@@ -6,3 +6,11 @@
 
 # Play video on open
 defaults write com.apple.QuickTimePlayerX MGPlayMovieOnOpen -bool true
+
+# Set recording quality
+# High:    MGCompressionPresetHighQuality
+# Maximum: MGCompressionPresetMaximumQuality
+defaults write com.apple.QuickTimePlayerX MGRecordingCompressionPresetIdentifier -string 'MGCompressionPresetMaximumQuality'
+
+# Show mouse clicks in screen recordings
+defaults write com.apple.QuickTimePlayerX MGScreenRecordingDocumentShowMouseClicksUserDefaultsKey -bool true
