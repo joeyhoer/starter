78f4573b09f32474e64397cb2e6135c5aa1cb1fd
Joey Hoer <joey@gaugeinteractive.com>
2015-06-26 16:22:54 -0400

    Adding antigen support to zsh

--- installers/shells.sh
+++ installers/shells.sh
@@ -10,10 +10,10 @@ sudo bash -c "which zsh bash >> /etc/shells"
 # Set default shell
 chsh -s $(which zsh)
 
-# Download Antigen
-curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > antigen.zsh
-source antigen.zsh
+# Create antigen directory
+mkdir -p ~/.antigen
+mkdir -p ~/.antigen/themes
 
-antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle powerline/powerline powerline/bindings/zsh
+# Download Antigen
+curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/.antigen/antigen.zsh
+source ~/.antigen/antigen.zsh
