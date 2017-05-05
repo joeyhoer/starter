add3330f65b620d95efb90ba17f91c3909f9e327
Joey Hoer <me@joeyhoer.com>
2017-05-05 12:58:55 -0400

    Adding Forklift configuration

---
+++ apps/forklift.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# ForkLift                                                                    #
+###############################################################################
+
+# Enable ForkLift Mini
+xattr -drs com.apple.quarentine /Applications/ForkLift.app
