d5da8ec6e84dedf1b03688bf6a42e71afa44a38a
Joey Hoer <me@joeyhoer.com>
2017-05-28 12:27:52 -0400

    Upgrading installers

--- installers/xcode.sh
+++ installers/xcode.sh
@@ -9,7 +9,7 @@ if ! $(xcode-select -p &>/dev/null); then
   xcode-select --install &>/dev/null
 
   # Wait until the Xcode Command Line Tools are installed
-  until $(xcode-select -p &>/dev/null); then
+  until $(xcode-select -p &>/dev/null); do
     sleep 5
   done
 fi
