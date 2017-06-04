1ef83794bf15ac0828106250839692ead5541f14
Joey <me@joeyhoer.com>
2017-06-03 23:02:44 -0400

    Replacing shell script Brewfile with brew bundle Brewfile

--- installers/homebrew.sh
+++ installers/homebrew.sh
@@ -12,8 +12,26 @@ fi
 # Schedule Homebrew Updates
 (crontab -l 2>/dev/null; echo "0 */6 * * * /usr/local/bin/brew update &>/dev/null") | crontab -
 
-# Install command-line tools using Homebrew
-source installers/homebrew/Brewfile
+# Install Homebrew Bundle
+brew tap Homebrew/bundle
 
-# Install GUI applications with Homebrew Cask
-source installers/homebrew/Caskfile
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we arere using the latest Homebrew
+brew update
+
+# Upgrade existing packages
+brew upgrade
+
+# Install CLI tools & GUI applications
+brew bundle --file=installers/homebrew/Brewfile
+
+# Remove outdated versions from the cellar
+brew cleanup && brew prune
+
+# Remove outdated cask versions from the cellar
+brew cask cleanup
