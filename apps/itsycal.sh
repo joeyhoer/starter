c43dd4f11c0501a0bd11ef53aea6a049b6b4b609
Joey Hoer <me@joeyhoer.com>
2017-06-12 12:17:42 -0400

    Add Itsycal support

---
+++ apps/itsycal.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Itsycal                                                                     #
+###############################################################################
+
+## General
+
+# Automatically check for updates
+defaults write com.mowglii.ItsycalApp SUEnableAutomaticChecks -bool true
+
+# First day of week
+defaults write com.mowglii.ItsycalApp WeekStartDOW -int 0
+
+# Event list shows X days
+defaults write com.mowglii.ItsycalApp ShowEventDays -int 7
+
+## Appearance
+
+# Use outline icon
+defaults write com.mowglii.ItsycalApp UseOutlineIcon -bool false
+
+# Show month in icon
+defaults write com.mowglii.ItsycalApp ShowMonthInIcon -bool false
+
+# Show day of week in icon
+defaults write com.mowglii.ItsycalApp ShowDayOfWeekInIcon -bool false
+
+# Hide date icon
+defaults write com.mowglii.ItsycalApp HideIcon -bool false
+
+# Highlight day(s) of week
+#   Day | S | M | T | W |  T |  F |  S
+# Value | 1 | 2 | 4 | 8 | 16 | 32 | 64
+# Add values together for each day selected
+# 65: Sunday (1) + Saturday (64)
+defaults write com.mowglii.ItsycalApp HighlightedDOWs -int 65
+
+# Show calendar weeks
+defaults write com.mowglii.ItsycalApp ShowWeeks -bool true
