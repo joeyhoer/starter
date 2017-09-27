201cd860497ca8a418e18bf03f465346de4a8c49
Joey Hoer <me@joeyhoer.com>
2017-09-27 09:32:31 -0400

    Upgrading Disk Utility

--- apps/disk-utility.sh
+++ apps/disk-utility.sh
@@ -7,3 +7,6 @@
 # Enable the debug menu in Disk Utility
 #defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
 defaults write com.apple.DiskUtility advanced-image-options -bool true
+
+# Show All Devices
+defaults write com.apple.DiskUtility SidebarShowAllDevices -bool true
