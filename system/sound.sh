2ffbd084e7009bb2fe56ab6e92c9ca9b7b9dc7f9
Joey Hoer <me@joeyhoer.com>
2017-09-27 09:31:59 -0400

    Updating sound preferences

--- system/sound.sh
+++ system/sound.sh
@@ -16,7 +16,17 @@ defaults write com.apple.systemsound com.apple.sound.beep.sound -string "/System
 defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -bool false
 
 # Play feedback when volume is changed
-defaults write NSGlobalDomain com.apple.sound.beep.feedback -bool true
+defaults write NSGlobalDomain com.apple.sound.beep.feedback -bool false
+
+# Disable flashing the screen when an alert sound occurs (accessibility)
+defaults write NSGlobalDomain com.apple.sound.beep.flash -bool false
+
+# Alert volume
+# Slider level:
+#  "75%": 0.7788008
+#  "50%": 0.6065307
+#  "25%": 0.4723665
+defaults write NSGlobalDomain com.apple.sound.beep.volume -float 0.6065307
 
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
