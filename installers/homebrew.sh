251ef7b55a70fe176ae691ca8d5c5066596a46fa
Joey Hoer <me@joeyhoer.com>
2018-01-28 17:43:44 -0500

    Updating homebrew auto update comments

--- installers/homebrew.sh
+++ installers/homebrew.sh
@@ -10,6 +10,9 @@ if ! hash brew 2>/dev/null; then
 fi
 
 # Schedule Homebrew Updates
+# This is better than HOMEBREW_AUTO_UPDATE_SECS
+# Consider disabling auto-updates with
+# export HOMEBREW_NO_AUTO_UPDATE=1
 cron_entry='0 */6 * * * /usr/local/bin/brew update &>/dev/null'
 if ! crontab -l | fgrep "$cron_entry" >/dev/null; then
   (crontab -l 2>/dev/null; echo "$cron_entry") | \
