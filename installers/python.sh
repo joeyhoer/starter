159102c86077498949d4f9d1a99d7527330ee9fa
Joey Hoer <me@joeyhoer.com>
2021-02-09 20:20:17 -0500

    Updating Python installer

--- installers/python.sh
+++ installers/python.sh
@@ -13,7 +13,7 @@ python_latest=$(pyenv install -l 2>/dev/null | awk '$1 ~ /^[0-9.]*$/ {latest=$1}
 pyenv install --skip-existing $python2_latest
 pyenv install --skip-existing $python_latest
 pyenv rehash
-pyenv global $python2_latest
+pyenv global $python_latest
 
 # pip *should* be installed at this point
 hash pip 2>/dev/null || echo "Please install pip before continuing"
@@ -29,5 +29,5 @@ pip_packages=(
 # Loop through each package individally because
 # any errors will stop all installations
 for package in "${pip_packages[@]}"; do
-  pip install --upgrade "$package"
+  pip install --upgrade "$package" --user
 done
